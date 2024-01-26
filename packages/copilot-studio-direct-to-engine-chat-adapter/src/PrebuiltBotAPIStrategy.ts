3c2c7e28d4efdeaa1c3fa7fc6c84ac53f3d2713c
William Wong <compulim@users.noreply.github.com>
2024-01-26 09:57:37 +0000

    Move to Copilot Studio SDK

---
+++ packages/copilot-studio-direct-to-engine-chat-adapter/src/PrebuiltBotAPIStrategy.ts
@@ -0,0 +1,42 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ */
+
+import { type HalfDuplexChatAdapterAPIStrategy } from './private/types/HalfDuplexChatAdapterAPIStrategy';
+
+type PrebuiltBotAPIStrategyInit = {
+  botIdentifier: string;
+  environmentEndpointURL: URL;
+  getTokenCallback: () => Promise<string>;
+};
+
+const API_VERSION = '2022-03-01-preview';
+
+export default class PublishedBotAPIStrategy implements HalfDuplexChatAdapterAPIStrategy {
+  constructor({ botIdentifier, environmentEndpointURL, getTokenCallback }: PrebuiltBotAPIStrategyInit) {
+    this.#getTokenCallback = getTokenCallback;
+
+    const url = new URL(`/powervirtualagents/prebuilt/authenticated/bots/${botIdentifier}/`, environmentEndpointURL);
+
+    url.searchParams.set('api-version', API_VERSION);
+
+    this.#baseURL = url;
+  }
+
+  #baseURL: URL;
+  #getTokenCallback: () => Promise<string>;
+
+  async #getHeaders() {
+    return { authorization: `Bearer ${await this.#getTokenCallback()}` };
+  }
+
+  public async prepareExecuteTurn(): ReturnType<HalfDuplexChatAdapterAPIStrategy['prepareExecuteTurn']> {
+    return { baseURL: this.#baseURL, headers: await this.#getHeaders() };
+  }
+
+  public async prepareStartNewConversation(): ReturnType<
+    HalfDuplexChatAdapterAPIStrategy['prepareStartNewConversation']
+  > {
+    return { baseURL: this.#baseURL, headers: await this.#getHeaders() };
+  }
+}
--- packages/copilot-studio-direct-to-engine-chat-adapter/src/index.ts
+++ packages/copilot-studio-direct-to-engine-chat-adapter/src/index.ts
@@ -3,8 +3,15 @@
  */
 
 import createHalfDuplexChatAdapter from './createHalfDuplexChatAdapter';
+import PrebuiltBotAPIStrategy from './PrebuiltBotAPIStrategy';
 import PublishedBotAPIStrategy from './PublishedBotAPIStrategy';
 import TestCanvasBotAPIStrategy from './TestCanvasBotAPIStrategy';
 import toDirectLineJS from './toDirectLineJS';
 
-export { createHalfDuplexChatAdapter, PublishedBotAPIStrategy, TestCanvasBotAPIStrategy, toDirectLineJS };
+export {
+  createHalfDuplexChatAdapter,
+  PrebuiltBotAPIStrategy,
+  PublishedBotAPIStrategy,
+  TestCanvasBotAPIStrategy,
+  toDirectLineJS
+};
